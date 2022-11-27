b8099899de3c0dce0f08b2e6c3c3dc3d669e8601
Mihail Kolev <36764541+BABAMIZAIKAL@users.noreply.github.com>
2022-11-27 18:06:16 +0200

    Updated parameter value

--- hw01.12.22/hw5_4.py
+++ hw01.12.22/hw5_4.py
@@ -39,5 +39,5 @@ def is_number(a):
         print("", end="")
     return False
 
-print(max_of_two(sum_list(input_nums(4)), sum_list([10, 0.1])))
+print(max_of_two(sum_list(input_nums(4)), sum_list(3)))
 print(max_of_two(sum_list([4, "AA@", 3.12, "1"]), "9.2"))
